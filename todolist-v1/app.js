58303387eca3e7bf00b9af96b01a1802dca169fa
Kevinprajapati <kevinprajapati2015@gmail.com>
2020-08-02 13:39:22 +0530

    version 1.0

---
+++ todolist-v1/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var items= [];
+const app = express();
+
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+
+app.get("/",function(req,res){
+    var today = new Date();
+   
+    var options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+        
+    };
+    var day = today.toLocaleDateString("en-US",options);
+        res.render("list",{listTitle:day,newItemLists:items});
+});
+
+
+app.post("/" ,function(req,res){
+     var item = req.body.newItem;
+
+    items.push(item);
+    res.redirect("/");
+  
+   
+});
+
+app.listen(2000,function(){
+    console.log("Server is running at port 2000....");
+})
